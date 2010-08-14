37dea96570d727dc6aef5fbdabd6b3bdf2f94b35
champ <champ@2eaad343-cf76-4621-bedb-4ec1b8031ff9>
2010-08-14 15:00:15 +0000

    More code clean up

--- src/sagan-stats.c
+++ src/sagan-stats.c
@@ -1,3 +1,33 @@
+/*
+** Copyright (C) 2009-2010 Softwink, Inc. 
+** Copyright (C) 2009-2010 Champ Clark III <champ@softwink.com>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License Version 2 as
+** published by the Free Software Foundation.  You may not use, modify or
+** distribute this program under any other version of the GNU General
+** Public License.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+/* sagan-stats.c 
+ *
+ * Simply dumps statistics of Sagan to the user or via sagan.log
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"             /* From autoconf */
+#endif
+
 #include <stdio.h>
 #include <string.h>
 #include "sagan.h"
