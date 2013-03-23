11652b346c663dcc312f76407fd3a9d74c9de861
Champ Clark III <cclark@quadrantsec.com>
2013-03-23 03:38:38 +0000

    Reload now does full reload (minus DNS cache)

--- src/sagan-signal.c
+++ src/sagan-signal.c
@@ -37,6 +37,13 @@
 #include "version.h"
 
 #include "sagan.h"
+#include "processors/sagan-blacklist.h"
+#include "processors/sagan-search.h"
+#include "processors/sagan-track-clients.h"
+
+#ifdef WITH_WEBSENSE
+#include "processors/sagan-websense.h"
+#endif
 
 #ifdef HAVE_LIBLOGNORM
 #include <liblognorm.h>
@@ -54,11 +61,19 @@ sbool sagan_unified2_flag;
 struct _SaganCounters *counters;
 struct _SaganDebug *debug;
 struct _SaganConfig *config;
+struct _Rule_Struct *rulestruct;
+struct _Class_Struct *classstruct;
+struct _Sagan_Processor_Generator *generator;
+struct _Sagan_Blacklist *SaganBlacklist;
+struct _Sagan_Nocase_Searchlist *SaganNocaseSearchlist;
+struct _Sagan_Case_Searchlist *SaganCaseSearchlist;
+struct _Sagan_Track_Clients *SaganTrackClients;
 
-struct rule_struct *rulestruct;
-struct class_struct *classstruct;
-struct ref_struct *refstruct;
-
+#ifdef WITH_WEBSENSE
+struct _Sagan_Websense_Ignore_List *SaganWebsenseIgnoreList;
+struct _Sagan_Websense_Queue *SaganWebsenseQueue;
+struct _Sagan_Websense_Cache *SaganWebsenseCache;
+#endif 
 
 pthread_mutex_t sig_mutex = PTHREAD_MUTEX_INITIALIZER;
 
@@ -103,12 +118,74 @@ if ( sagan_unified2_flag ) Unified2CleanExit(config);
                    pthread_mutex_lock(&sig_mutex);
    		   Sagan_Log(0, "[Reloading Sagan version %s.]-------", VERSION);
 
-		      /* Reset counters */
-		   counters->refcount=0; counters->classcount=0; counters->rulecount=0; counters->ruletotal=0;
-		   
-		   /* Re-load everything */
+		   /* Reset counters */
+		   counters->refcount=0; 
+		   counters->classcount=0;
+		   counters->rulecount=0; 
+		   counters->ruletotal=0;
+		   counters->genmapcount=0;
+
+		   memset(rulestruct, 0, sizeof(_Rule_Struct));
+		   memset(classstruct, 0, sizeof(_Class_Struct));
+		   memset(generator, 0, sizeof(_Sagan_Processor_Generator)); 
+
+		   /* Re-load primary configuration (rules/classifictions/etc) */
 
 		  Load_Config();
+
+		  if (config->blacklist_flag) {
+		     counters->blacklist_count=0;
+		     memset(SaganBlacklist, 0, sizeof(_Sagan_Blacklist));
+		     Sagan_Blacklist_Load();
+		     Sagan_Log(0, "Reloaded Blacklist. [File: %s | Count: %d | Parse Depth: %d]", config->blacklist_file, counters->blacklist_count, config->blacklist_parse_depth);
+		     }
+
+		  if (config->search_nocase_flag) {
+                     counters->search_nocase_count=0;
+		     memset(SaganNocaseSearchlist, 0, sizeof(_Sagan_Nocase_Searchlist));
+		     Sagan_Search_Load(1);
+		     Sagan_Log(0, "Reloaded Search [nocase]. [File: %s | Count: %d]", config->search_nocase_file, counters->search_nocase_count); 
+		     }
+
+		  if (config->search_case_flag) {
+		     counters->search_case_count=0;
+		     memset(SaganCaseSearchlist, 0, sizeof(_Sagan_Case_Searchlist));
+		     Sagan_Search_Load(2);
+		     Sagan_Log(0, "Reloaded Search. [File: %s | Count: %d]", config->search_nocase_file, counters->search_nocase_count);
+		     }
+
+
+		  if (config->sagan_track_clients_flag) { 
+		     counters->track_clients_client_count = 0;
+		     counters->track_clients_down = 0; 
+		     memset(SaganTrackClients, 0, sizeof(_Sagan_Track_Clients));
+		     Sagan_Log(0, "Reset Sagan Track Client.");
+		     }
+
+/*		  DNS Cache *not currently global* DEBUG 
+		  if (config->syslog_src_lookup) { 
+		     counters->dns_cache_count=0;
+		     counters->dns_miss_count=0;
+		     }
+*/
+
+#ifdef WITH_WEBSENSE
+		  if ( config->websense_flag ) {
+		     counters->websense_cache_count=0;
+		     counters->websense_cache_hit=0;
+		     counters->websense_ignore_hit=0;
+		     counters->websense_postive_hit=0;
+		     memset(SaganWebsenseIgnoreList, 0, sizeof(_Sagan_Websense_Ignore_List));
+		     memset(SaganWebsenseQueue, 0, sizeof(_Sagan_Websense_Queue));
+
+		     SaganWebsenseCache = malloc(config->websense_max_cache * sizeof(struct _Sagan_Websense_Cache));
+		     memset(SaganWebsenseCache, 0, sizeof(_Sagan_Websense_Cache));
+
+		     config->websense_last_time = atol(config->sagan_startutime);
+ 		     Sagan_Websense_Ignore_List();
+		     Sagan_Log(0, "Reset Websense Processor.");
+		     }
+#endif
 
                   pthread_mutex_unlock(&sig_mutex);
 		  
