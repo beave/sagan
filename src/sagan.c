f92c7415e953474a6da7c6fcf80276bddefcd780
Champ Clark III <cclark@quadrantsec.com>
2013-10-22 02:11:03 +0000

    strlcpy buffer corruption

--- src/sagan.c
+++ src/sagan.c
@@ -799,6 +799,7 @@ while(fd != NULL) {
 
 	          pthread_mutex_lock(&SaganProcWorkMutex);
 
+/*
 		  strlcpy(SaganProcSyslog[proc_msgslot].syslog_host, syslog_host, sizeof(SaganProcSyslog[proc_msgslot].syslog_host)); 
 		  strlcpy(SaganProcSyslog[proc_msgslot].syslog_facility, syslog_facility, sizeof(SaganProcSyslog[proc_msgslot].syslog_facility));
 		  strlcpy(SaganProcSyslog[proc_msgslot].syslog_priority, syslog_priority, sizeof(SaganProcSyslog[proc_msgslot].syslog_priority)); 
@@ -808,6 +809,17 @@ while(fd != NULL) {
 		  strlcpy(SaganProcSyslog[proc_msgslot].syslog_time, syslog_time, sizeof(SaganProcSyslog[proc_msgslot].syslog_time)); 
 		  strlcpy(SaganProcSyslog[proc_msgslot].syslog_program, syslog_program, sizeof(SaganProcSyslog[proc_msgslot].syslog_program));
 		  strlcpy(SaganProcSyslog[proc_msgslot].syslog_message, syslog_msg, sizeof(SaganProcSyslog[proc_msgslot].syslog_message)); 
+*/
+
+                  snprintf(SaganProcSyslog[proc_msgslot].syslog_host, sizeof(SaganProcSyslog[proc_msgslot].syslog_host)-1, "%s", syslog_host);
+                  snprintf(SaganProcSyslog[proc_msgslot].syslog_facility, sizeof(SaganProcSyslog[proc_msgslot].syslog_facility)-1, "%s", syslog_facility);
+                  snprintf(SaganProcSyslog[proc_msgslot].syslog_priority, sizeof(SaganProcSyslog[proc_msgslot].syslog_priority)-1, "%s", syslog_priority);
+                  snprintf(SaganProcSyslog[proc_msgslot].syslog_level, sizeof(SaganProcSyslog[proc_msgslot].syslog_level)-1, "%s", syslog_level);
+                  snprintf(SaganProcSyslog[proc_msgslot].syslog_tag, sizeof(SaganProcSyslog[proc_msgslot].syslog_tag)-1, "%s", syslog_tag);
+                  snprintf(SaganProcSyslog[proc_msgslot].syslog_date, sizeof(SaganProcSyslog[proc_msgslot].syslog_date)-1, "%s", syslog_date);
+                  snprintf(SaganProcSyslog[proc_msgslot].syslog_time, sizeof(SaganProcSyslog[proc_msgslot].syslog_time)-1, "%s", syslog_time);
+                  snprintf(SaganProcSyslog[proc_msgslot].syslog_program, sizeof(SaganProcSyslog[proc_msgslot].syslog_program)-1, "%s", syslog_program);
+                  snprintf(SaganProcSyslog[proc_msgslot].syslog_message, sizeof(SaganProcSyslog[proc_msgslot].syslog_message)-1, "%s", syslog_msg);
 
 	          proc_msgslot++;
 
