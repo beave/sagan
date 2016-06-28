8b23754b273351be857cce5232ca33d62ccf7a65
Champ Clark III <champ@dev.quadrantsec.com>
2016-06-28 03:33:51 +0000

    Fix bug when Sagan cannot open a file.

--- src/sagan.c
+++ src/sagan.c
@@ -783,20 +783,33 @@ int main(int argc, char **argv)
 
             if (( fd = fopen(config->sagan_fifo, "r" )) == NULL )
                 {
-                    /* try to create it */
-                    if (mkfifo(config->sagan_fifo, 0700) == -1)
+
+                    if ( config->sagan_is_file == 0 )
                         {
-                            Sagan_Log(S_ERROR, "Could not create fifo %s. Abort!", config->sagan_fifo);
+
+                            /* try to create it */
+
+                            Sagan_Log(S_NORMAL, "Fifo not found, creating it (%s).", config->sagan_fifo);
+
+                            if (mkfifo(config->sagan_fifo, 0700) == -1)
+                                {
+                                    Sagan_Log(S_ERROR, "Could not create FIFO '%s'. Abort!", config->sagan_fifo);
+                                }
+
+                            fd = fopen(config->sagan_fifo, "r");
+
+                            if ( fd == NULL )
+                                {
+                                    Sagan_Log(S_ERROR, "Error opening %s. Abort!", config->sagan_fifo);
+                                }
+
                         }
                     else
                         {
-                            Sagan_Log(S_NORMAL, "Fifo not found, creating it (%s).", config->sagan_fifo);
+
+                            Sagan_Log(S_ERROR, "Could not open file '%s'. Abort!", config->sagan_fifo);
                         }
-                    fd = fopen(config->sagan_fifo, "r");
-                    if ( fd == NULL )
-                        {
-                            Sagan_Log(S_ERROR, "Error opening %s. Abort!", config->sagan_fifo);
-                        }
+
                 }
 
             if ( config->sagan_is_file == 0 )
