911e895d2d246c5605bd6fd42da20cb2a3e5f75b
Champ Clark III <champ@dev.quadrantsec.com>
2017-01-15 20:44:09 +0000

    Fixed some issues with autotools issues (not remove \n after version extract, etc)

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# This generates makefiles, configure, etc.
+# This generates Makefiles, ./configure, etc.
 
-#libtoolize --force
-#aclocal -I m4
-#autoheader -I m4
 autoreconf -vfi -I m4
