a23a0837da8021efcf52cf0bd9da0ca9fd758f31
Champ Clark III <champ@dev.quadrantsec.com>
2017-07-22 19:56:29 +0000

    Argh! Damn find/replace screw up resolved (i hope).

--- src/after.c
+++ src/after.c
@@ -83,7 +83,8 @@ sbool After_By_Src ( int rule_position, char *ip_src, uint32_t ip_src_u32 )
 
     for (i = 0; i < counters_ipc->after_count_by_src; i++ ) {
 
-        if ( afterbysrc_ipc[i].ipsrc == ip_src_u32  && !strcmp(afterbysrc_ipc[i].sid, rulestruct[rule_position].s_sid )) {
+        if ( afterbysrc_ipc[i].ipsrc == ip_src_u32 && 
+	     !strcmp(afterbysrc_ipc[i].sid, rulestruct[rule_position].s_sid )) {
 
             after_flag = true;
 
@@ -95,7 +96,9 @@ sbool After_By_Src ( int rule_position, char *ip_src, uint32_t ip_src_u32 )
 
             after_oldtime = atol(timet) - afterbysrc_ipc[i].utime;
 
-            if ( after_oldtime > rulestruct[rule_position].after_seconds || afterbysrc_ipc[i].count == 0 ) {
+            if ( after_oldtime > rulestruct[rule_position].after_seconds || 
+		 afterbysrc_ipc[i].count == 0 ) {
+
                 afterbysrc_ipc[i].count=1;
                 afterbysrc_ipc[i].utime = atol(timet);
                 after_log_flag = true;
@@ -142,9 +145,8 @@ sbool After_By_Src ( int rule_position, char *ip_src, uint32_t ip_src_u32 )
 
     }
 
-return(after_log_flag);
+    return(after_log_flag);
 }
-
 
 /************************/
 /* After by Destination */
@@ -170,28 +172,33 @@ sbool After_By_Dst ( int rule_position, char *ip_dst, uint32_t ip_dst_u32 )
 
     for (i = 0; i < counters_ipc->after_count_by_dst; i++ ) {
 
-        if ( afterbydst_ipc[rule_position].ipdst == ip_dst_u32  && !strcmp(afterbydst_ipc[rule_position].sid, rulestruct[rule_position].s_sid )) {
+        if ( afterbydst_ipc[i].ipdst == ip_dst_u32 && 
+	     !strcmp(afterbydst_ipc[i].sid, rulestruct[rule_position].s_sid )) {
 
             after_flag = true;
 
             File_Lock(config->shm_after_by_dst);
             pthread_mutex_lock(&After_By_Dst_Mutex);
 
-            afterbydst_ipc[rule_position].count++;
-            afterbydst_ipc[rule_position].total_count++;
+            afterbydst_ipc[i].count++;
+            afterbydst_ipc[i].total_count++;
 
-            after_oldtime = atol(timet) - afterbydst_ipc[rule_position].utime;
-            if ( after_oldtime > rulestruct[rule_position].after_seconds || afterbydst_ipc[rule_position].count == 0 ) {
-                afterbydst_ipc[rule_position].count=1;
-                afterbydst_ipc[rule_position].utime = atol(timet);
+            after_oldtime = atol(timet) - afterbydst_ipc[i].utime;
+
+            if ( after_oldtime > rulestruct[rule_position].after_seconds || 
+		 afterbydst_ipc[i].count == 0 ) {
+
+                afterbydst_ipc[i].count=1;
+                afterbydst_ipc[i].utime = atol(timet);
                 after_log_flag = true;
             }
 
-            if ( rulestruct[rule_position].after_count < afterbydst_ipc[rule_position].count ) {
+            if ( rulestruct[rule_position].after_count < afterbydst_ipc[i].count ) {
+
                 after_log_flag = false;
 
                 if ( debug->debuglimits ) {
-                    Sagan_Log(S_NORMAL, "After SID %s by destination IP address. [%s]", afterbydst_ipc[rule_position].sid, ip_dst);
+                    Sagan_Log(S_NORMAL, "After SID %s by destination IP address. [%s]", afterbydst_ipc[i].sid, ip_dst);
                 }
 
                 counters->after_total++;
@@ -229,6 +236,7 @@ sbool After_By_Dst ( int rule_position, char *ip_dst, uint32_t ip_dst_u32 )
     }
 
     return(after_log_flag);
+
 }
 
 /*********************/
@@ -253,70 +261,73 @@ sbool After_By_Username( int rule_position, char *normalize_username )
     now=localtime(&t);
     strftime(timet, sizeof(timet), "%s",  now);
 
-    /* Check array for matching username / sid */
+        /* Check array for matching username / sid */
 
-    for (i = 0; i < counters_ipc->after_count_by_username; i++ ) {
+        for (i = 0; i < counters_ipc->after_count_by_username; i++ ) {
 
-        if ( !strcmp(afterbyusername_ipc[rule_position].username, normalize_username) &&
-             !strcmp(afterbyusername_ipc[rule_position].sid, rulestruct[rule_position].s_sid )) {
+            if ( !strcmp(afterbyusername_ipc[i].username, normalize_username) && 
+		 !strcmp(afterbyusername_ipc[i].sid, rulestruct[rule_position].s_sid )) {
 
-            after_flag = true;
+                after_flag = true;
 
-            File_Lock(config->shm_after_by_username);
-            pthread_mutex_lock(&After_By_Username_Mutex);
+                File_Lock(config->shm_after_by_username);
+                pthread_mutex_lock(&After_By_Username_Mutex);
 
-            afterbyusername_ipc[rule_position].count++;
-            afterbyusername_ipc[rule_position].total_count++;
+                afterbyusername_ipc[i].count++;
+                afterbyusername_ipc[i].total_count;
 
-            after_oldtime = atol(timet) - afterbyusername_ipc[rule_position].utime;
-            if ( after_oldtime > rulestruct[rule_position].after_seconds || afterbysrc_ipc[rule_position].count == 0 ) {
-                afterbyusername_ipc[rule_position].count=1;
-                afterbyusername_ipc[rule_position].utime = atol(timet);
-                after_log_flag = true;
-            }
+                after_oldtime = atol(timet) - afterbyusername_ipc[i].utime;
 
-            if ( rulestruct[rule_position].after_count < afterbyusername_ipc[rule_position].count ) {
-                after_log_flag = false;
+                if ( after_oldtime > rulestruct[rule_position].after_seconds || 
+		     afterbysrc_ipc[i].count == 0 ) {
 
-                if ( debug->debuglimits ) {
-                    Sagan_Log(S_NORMAL, "After SID %s by_username / by_string. [%s]", afterbyusername_ipc[rule_position].sid, normalize_username);
+                    afterbyusername_ipc[i].count=1;
+                    afterbyusername_ipc[i].utime = atol(timet);
+                    after_log_flag = true;
                 }
 
-                counters->after_total++;
+                if ( rulestruct[rule_position].after_count < afterbyusername_ipc[i].count ) {
+                    after_log_flag = false;
+
+                    if ( debug->debuglimits ) {
+                        Sagan_Log(S_NORMAL, "After SID %s by_username. [%s]", afterbyusername_ipc[i].sid, normalize_username);
+                    }
+
+                    counters->after_total++;
+
+                }
+
+                pthread_mutex_unlock(&After_By_Username_Mutex);
+                File_Unlock(config->shm_after_by_username);
 
             }
-
-            pthread_mutex_unlock(&After_By_Username_Mutex);
-            File_Unlock(config->shm_after_by_username);
-
         }
-    }
 
-    /* If not found, add to the username array */
+        /* If not found, add to the username array */
 
-    if ( after_flag == false ) {
+        if ( after_flag == false ) {
 
-        if ( Clean_IPC_Object(AFTER_BY_DST) == 0 ) {
+            if ( Clean_IPC_Object(AFTER_BY_DST) == 0 ) {
 
-            File_Lock(config->shm_after_by_username);
-            pthread_mutex_lock(&After_By_Username_Mutex);
+                File_Lock(config->shm_after_by_username);
+                pthread_mutex_lock(&After_By_Username_Mutex);
 
-            strlcpy(afterbyusername_ipc[counters_ipc->after_count_by_username].username, normalize_username, sizeof(afterbyusername_ipc[counters_ipc->after_count_by_username].username));
-            strlcpy(afterbyusername_ipc[counters_ipc->after_count_by_username].sid, rulestruct[rule_position].s_sid, sizeof(afterbyusername_ipc[counters_ipc->after_count_by_username].sid));
-            afterbyusername_ipc[counters_ipc->after_count_by_username].count = 1;
-            afterbyusername_ipc[counters_ipc->after_count_by_username].utime = atol(timet);
-            afterbyusername_ipc[counters_ipc->after_count_by_username].expire = rulestruct[rule_position].after_seconds;
+                strlcpy(afterbyusername_ipc[counters_ipc->after_count_by_username].username, normalize_username, sizeof(afterbyusername_ipc[counters_ipc->after_count_by_username].username));
+                strlcpy(afterbyusername_ipc[counters_ipc->after_count_by_username].sid, rulestruct[rule_position].s_sid, sizeof(afterbyusername_ipc[counters_ipc->after_count_by_username].sid));
+                afterbyusername_ipc[counters_ipc->after_count_by_username].count = 1;
+                afterbyusername_ipc[counters_ipc->after_count_by_username].utime = atol(timet);
+                afterbyusername_ipc[counters_ipc->after_count_by_username].expire = rulestruct[rule_position].after_seconds;
 
-            counters_ipc->after_count_by_username++;
+                counters_ipc->after_count_by_username++;
 
-            pthread_mutex_unlock(&After_By_Username_Mutex);
-            File_Unlock(config->shm_after_by_username);
+                pthread_mutex_unlock(&After_By_Username_Mutex);
+                File_Unlock(config->shm_after_by_username);
+            }
         }
-    }
 
-    return(after_log_flag);
+return(after_log_flag);
 
-}
+} /* End of After */
 
 /***************************/
 /* After by source IP port */
@@ -340,31 +351,35 @@ sbool After_By_SrcPort( int rule_position, uint32_t ip_srcport_u32 )
     now=localtime(&t);
     strftime(timet, sizeof(timet), "%s",  now);
 
-    /* Check array for matching src / sid */
 
     for (i = 0; i < counters_ipc->after_count_by_srcport; i++ ) {
 
-        if ( afterbysrcport_ipc[rule_position].ipsrcport == ip_srcport_u32 && !strcmp(afterbysrcport_ipc[rule_position].sid, rulestruct[rule_position].s_sid )) {
+        if ( afterbysrcport_ipc[i].ipsrcport == ip_srcport_u32 &&
+             !strcmp(afterbysrcport_ipc[i].sid, rulestruct[rule_position].s_sid )) {
+
             after_flag = true;
 
             File_Lock(config->shm_after_by_srcport);
             pthread_mutex_lock(&After_By_Src_Port_Mutex);
 
-            afterbysrcport_ipc[rule_position].count++;
-            afterbysrcport_ipc[rule_position].total_count++;
+            afterbysrcport_ipc[i].count++;
+            afterbysrcport_ipc[i].total_count++;
 
-            after_oldtime = atol(timet) - afterbysrcport_ipc[rule_position].utime;
-            if ( after_oldtime > rulestruct[rule_position].after_seconds || afterbysrc_ipc[rule_position].count == 0 ) {
-                afterbysrcport_ipc[rule_position].count=1;
-                afterbysrcport_ipc[rule_position].utime = atol(timet);
+            after_oldtime = atol(timet) - afterbysrcport_ipc[i].utime;
+
+            if ( after_oldtime > rulestruct[rule_position].after_seconds || 
+		 afterbysrc_ipc[i].count == 0 ) {
+
+                afterbysrcport_ipc[i].count=1;
+                afterbysrcport_ipc[i].utime = atol(timet);
                 after_log_flag = true;
             }
 
-            if ( rulestruct[rule_position].after_count < afterbysrcport_ipc[rule_position].count ) {
+            if ( rulestruct[rule_position].after_count < afterbysrcport_ipc[i].count ) {
                 after_log_flag = false;
 
                 if ( debug->debuglimits ) {
-                    Sagan_Log(S_NORMAL, "After SID %s by source IP port. [%d]", afterbysrcport_ipc[rule_position].sid, ip_srcport_u32);
+                    Sagan_Log(S_NORMAL, "After SID %s by source IP port. [%d]", afterbysrcport_ipc[i].sid, ip_srcport_u32);
                 }
 
                 counters->after_total++;
@@ -425,31 +440,36 @@ sbool After_By_DstPort( int rule_position, uint32_t ip_dstport_u32 )
     now=localtime(&t);
     strftime(timet, sizeof(timet), "%s",  now);
 
-    /* Check array for matching dst / sid */
 
     for (i = 0; i < counters_ipc->after_count_by_dstport; i++ ) {
 
-        if ( afterbydstport_ipc[rule_position].ipdstport == ip_dstport_u32 && !strcmp(afterbydstport_ipc[rule_position].sid, rulestruct[rule_position].s_sid )) {
+        if ( afterbydstport_ipc[i].ipdstport == ip_dstport_u32 &&
+             !strcmp(afterbydstport_ipc[i].sid, rulestruct[rule_position].s_sid )) {
+
             after_flag = true;
 
             File_Lock(config->shm_after_by_dstport);
             pthread_mutex_lock(&After_By_Dst_Port_Mutex);
 
-            afterbydstport_ipc[rule_position].count++;
-            afterbydstport_ipc[rule_position].total_count++;
+            afterbydstport_ipc[i].count++;
+            afterbydstport_ipc[i].total_count++;
 
-            after_oldtime = atol(timet) - afterbydstport_ipc[rule_position].utime;
-            if ( after_oldtime > rulestruct[rule_position].after_seconds || afterbydst_ipc[rule_position].count == 0 ) {
-                afterbydstport_ipc[rule_position].count=1;
-                afterbydstport_ipc[rule_position].utime = atol(timet);
+            after_oldtime = atol(timet) - afterbydstport_ipc[i].utime;
+
+            if ( after_oldtime > rulestruct[rule_position].after_seconds ||
+                 afterbysrc_ipc[i].count == 0 ) {
+
+                afterbydstport_ipc[i].count=1;
+                afterbydstport_ipc[i].utime = atol(timet);
                 after_log_flag = true;
+
             }
 
-            if ( rulestruct[rule_position].after_count < afterbydstport_ipc[rule_position].count ) {
+            if ( rulestruct[rule_position].after_count < afterbydstport_ipc[i].count ) {
                 after_log_flag = false;
 
                 if ( debug->debuglimits ) {
-                    Sagan_Log(S_NORMAL, "After SID %s by destination IP port. [%d]", afterbydstport_ipc[rule_position].sid, ip_dstport_u32);
+                    Sagan_Log(S_NORMAL, "After SID %s by destination IP port. [%d]", afterbydstport_ipc[i].sid, ip_dstport_u32);
                 }
 
                 counters->after_total++;
@@ -465,7 +485,7 @@ sbool After_By_DstPort( int rule_position, uint32_t ip_dstport_u32 )
 
     if ( after_flag == false ) {
 
-        if ( Clean_IPC_Object(AFTER_BY_SRCPORT) == 0 ) {
+        if ( Clean_IPC_Object(AFTER_BY_DSTPORT) == 0 ) {
 
             File_Lock(config->shm_after_by_dstport);
             pthread_mutex_lock(&After_By_Dst_Port_Mutex);
@@ -487,4 +507,3 @@ sbool After_By_DstPort( int rule_position, uint32_t ip_dstport_u32 )
     return(after_log_flag);
 
 }
-
