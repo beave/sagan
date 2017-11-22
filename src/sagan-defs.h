c2de67393509f1b61df0736af32250cc5f20712a
Champ Clark III <champ@dev.quadrantsec.com>
2017-11-22 04:48:44 +0000

    Adjust MAXIP for IPv6

--- src/sagan-defs.h
+++ src/sagan-defs.h
@@ -81,8 +81,8 @@ typedef void json_object;
 #define MAX_REFERENCE		10		/* Max references within a rule */
 #define MAX_PARSE_IP		10		/* Max IP to collect form log line via parse.c */
 
-#define MAXIP			64		/* Max IP length */
-#define MAXIPBIT	     	16		/* Max IP length in bytes */
+#define MAXIP			46		/* Max IP length */
+#define MAXIPBIT	     	46		/* Max IP length in bytes */
 #define MAXSELECTOR		64		/* Max tracking selector length */
 
 #define LOCKFILE 		"/var/run/sagan/sagan.pid"
