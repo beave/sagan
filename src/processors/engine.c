c685f3637930de20aca707baf4af89bc7141d148
Champ Clark III <champ@dev.quadrantsec.com>
2020-06-10 04:31:58 +0000

    Fix for JSON issue after memory fixes...

--- src/processors/engine.c
+++ src/processors/engine.c
@@ -198,6 +198,8 @@ int Sagan_Engine ( _Sagan_Proc_Syslog *SaganProcSyslog_LOCAL, bool dynamic_rule_
     char *sha1_hash = NULL;
     char *sha256_hash = NULL;
 
+    char json_normalize[JSON_MAX_SIZE] = { 0 }; 
+
     /* These do not need to be reset each time as they are _only_
      * set through normalize */
 
@@ -224,6 +226,12 @@ int Sagan_Engine ( _Sagan_Proc_Syslog *SaganProcSyslog_LOCAL, bool dynamic_rule_
     /* Outside the WITH_BLUEDOT because we use it in passing to Send_Alert() */
 
     char bluedot_json[BLUEDOT_JSON_SIZE] = { 0 };
+
+    /* Set a default for liblognorm JSON.  In case it's not used */
+
+    json_normalize[0] = '"';
+    json_normalize[1] = '"';
+    json_normalize[2] = '\0'; 
 
 #ifdef HAVE_LIBLOGNORM
 
@@ -572,6 +580,8 @@ int Sagan_Engine ( _Sagan_Proc_Syslog *SaganProcSyslog_LOCAL, bool dynamic_rule_
                                     liblognorm_status = -1;
 
                                     Normalize_Liblognorm(SaganProcSyslog_LOCAL->syslog_message, &SaganNormalizeLiblognorm);
+
+				    strlcpy(json_normalize, SaganNormalizeLiblognorm.json_normalize, sizeof(json_normalize)); 
 
                                     if ( SaganNormalizeLiblognorm.ip_src[0] != '0'  ||
                                             SaganNormalizeLiblognorm.ip_dst[0] != '0'  ||
@@ -1376,7 +1386,7 @@ int Sagan_Engine ( _Sagan_Proc_Syslog *SaganProcSyslog_LOCAL, bool dynamic_rule_
                                                         {
 
                                                             Send_Alert(SaganProcSyslog_LOCAL,
-                                                                       SaganNormalizeLiblognorm.json_normalize,
+                                                                       json_normalize,
                                                                        processor_info_engine,
                                                                        ip_src,
                                                                        ip_dst,
